5e376892517c4853f3f6a221fc5bc0c35fed5806
sunadi amarasinghe <139256776+sunadi02@users.noreply.github.com>
2026-09-19 14:13:13 +0530

    Update the route comment on listBBPackageDetails to show the correct TMF gateway path

--- src/APIs/listBBPackageDetails/routes/bbPackageDetailsRoutes.js
+++ src/APIs/listBBPackageDetails/routes/bbPackageDetailsRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { listBBPackageDetails } = require('../controllers/bbPackageDetailsController');
 
-// GET /internal-api/product-catalog/v1/bbPackageDetails?code=ADSL-WFP
+// GET /tmf-api/productCatalogManagement/v4/bbPackageDetails?code=ADSL-WFP
 router.get('/', listBBPackageDetails);
 
 module.exports = router;
