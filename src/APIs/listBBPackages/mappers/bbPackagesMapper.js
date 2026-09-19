6b91ecc10b97b8448aa1dbf10744b386dd6b91b8
sunadi amarasinghe <139256776+sunadi02@users.noreply.github.com>
2026-09-19 13:49:10 +0530

    Make the listBBPackages mapper self-contained instead of importing shared helpers

--- src/APIs/listBBPackages/mappers/bbPackagesMapper.js
+++ src/APIs/listBBPackages/mappers/bbPackagesMapper.js
@@ -1,4 +1,32 @@
-const { mapOfferingToResource } = require('../../../mappers/productOfferingMapper');
+// Mapper for listBBPackages - self-contained, no shared folder outside APIs/
+// per the team's file structure rule.
+
+function toCharacteristics(obj) {
+  return Object.entries(obj)
+    .filter(([, value]) => value !== undefined)
+    .map(([name, value]) => ({ name, value }));
+}
+
+function buildResource({ id, type, baseType, extra = {} }) {
+  return {
+    id,
+    href: `/${type.charAt(0).toLowerCase()}${type.slice(1)}/${id}`,
+    '@type': type,
+    '@baseType': baseType || type,
+    ...extra,
+  };
+}
+
+function mapOfferingToResource(offering, extraCharacteristics = {}) {
+  return buildResource({
+    id: offering.packageId,
+    type: 'ProductOffering',
+    extra: {
+      name: offering.name,
+      productOfferingCharacteristic: toCharacteristics(extraCharacteristics),
+    },
+  });
+}
 
 function mapBBPackages({ upgrades, downgrades }) {
   return [
