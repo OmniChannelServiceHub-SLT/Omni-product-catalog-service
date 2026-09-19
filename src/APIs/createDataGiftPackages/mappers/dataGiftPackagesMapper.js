7cc163afa488e0c3ef0a1b0fe1f762a9fcac8133
sunadi amarasinghe <139256776+sunadi02@users.noreply.github.com>
2026-09-19 13:49:41 +0530

    Make the createDataGiftPackages mapper self-contained instead of importing shared helpers

--- src/APIs/createDataGiftPackages/mappers/dataGiftPackagesMapper.js
+++ src/APIs/createDataGiftPackages/mappers/dataGiftPackagesMapper.js
@@ -1,4 +1,37 @@
-const { mapOfferingToResource } = require('../../../mappers/productOfferingMapper');
+// Mapper for createDataGiftPackages - self-contained, no shared folder
+// outside APIs/ per the team's file structure rule.
+
+function toCharacteristics(obj) {
+  return Object.entries(obj)
+    .filter(([, value]) => value !== undefined)
+    .map(([name, value]) => ({ name, value }));
+}
+
+function buildResource({ id, type, baseType, extra = {} }) {
+  return {
+    id,
+    href: `/${type.charAt(0).toLowerCase()}${type.slice(1)}/${id}`,
+    '@type': type,
+    '@baseType': baseType || type,
+    ...extra,
+  };
+}
+
+function mapOfferingToResource(offering) {
+  return buildResource({
+    id: offering.packageId,
+    type: 'ProductOffering',
+    extra: {
+      name: offering.name,
+      description: offering.description,
+      productOfferingPrice: toCharacteristics({
+        prePrice: offering.prePrice,
+        postPrice: offering.postPrice,
+        taxValue: offering.taxValue,
+      }),
+    },
+  });
+}
 
 function mapDataGiftPackages(offerings) {
   return offerings.map((o) => mapOfferingToResource(o));
